d7cafad6ff8acaca350da0e78c39454a1d0d551d
Davis <61391491+DavisThorne@users.noreply.github.com>
2023-08-17 22:31:53 +0100

    Added Blackjack

--- modules/casino.py
+++ modules/casino.py
@@ -51,13 +51,13 @@ class Casino(commands.Cog, name="Casino"):
 
         if len(set(spin_results)) == 1:
             winnings = (bet * 5) - bet
-            await ctx.respond(f"All three numbers were equal, you win: £{winnings}")
+            await ctx.respond(f"All three numbers were equal, you win: £{winnings:,}")
         elif len(set(spin_results)) == 2:
             winnings = (bet * 3) - bet
-            await ctx.respond(f"Two of the numbers were equal, you win: £{winnings}")
+            await ctx.respond(f"Two of the numbers were equal, you win: £{winnings:,}")
         else:
             winnings = 0 - bet
-            await ctx.respond(f"No numbers were equal, you lost: £{bet}")
+            await ctx.respond(f"No numbers were equal, you lost: £{bet:,}")
 
         await self.update_user_balance(user_id, winnings)
 
@@ -77,12 +77,73 @@ class Casino(commands.Cog, name="Casino"):
         result = random.choice(["Heads", "Tails"])
         if result == choices:
             winnings = bet * 2
-            await ctx.respond(f"The coin landed on {result}, you win: £{winnings}")
+            await ctx.respond(f"The coin landed on {result}, you win: £{winnings:,}")
         else:
             winnings = 0 - bet
-            await ctx.respond(f"The coin landed on {result}, you lost: £{bet}")
+            await ctx.respond(f"The coin landed on {result}, you lost: £{bet:,}")
 
         await self.update_user_balance(user_id, winnings)
+
+    @commands.slash_command(name="blackjack", description="Plays Blackjack")
+    async def blackjack(self, ctx,
+                        bet: discord.Option(int, required=True)):
+        user_id = ctx.author.id
+        user_data = await self.get_user_data(user_id)
+        if not user_data:
+            await self.add_user_db(user_id, ctx)
+            return
+        deck = [i for i in range(1, 11)] * 4
+        random.shuffle(deck)
+        dealer_hand = [deck.pop(), deck.pop()]
+        player_hand = [deck.pop(), deck.pop()]
+        dealer_total = sum(dealer_hand)
+        player_total = sum(player_hand)
+        dealer_string = f"Dealer's Hand: {dealer_hand[0]}, ?"
+        dealer_string_unhidden = f"Dealer's Hand: {dealer_hand[0]}, {dealer_hand[1]}"
+        player_string = f"Your Hand: {player_hand[0]}, {player_hand[1]}"
+        if player_total == 21:
+            await ctx.respond(f"{dealer_string}\n{player_string}\nYou got Blackjack! You win: £{bet * 2:,}")
+            await self.update_user_balance(user_id, bet * 2)
+            return
+        while player_total < 21:
+            await ctx.respond(f"{dealer_string}\n{player_string}\nWould you like to hit or stand?",
+                              ephemeral=True)
+            hit_or_stand = await self.client.wait_for("message", check=lambda m: m.author == ctx.author)
+            if hit_or_stand.content.lower() == "hit":
+                player_hand.append(deck.pop())
+                player_total = sum(player_hand)
+                player_string += f", {player_hand[-1]}"
+                await hit_or_stand.delete()
+                if player_total > 21:
+                    await ctx.respond(f"{dealer_string_unhidden}\n{player_string}\nYou busted! You lost: £{bet:,}")
+                    await self.update_user_balance(user_id, 0 - bet)
+                    return
+                elif len(player_hand) == 5 and player_total <= 21:
+                    await ctx.respond(f"{dealer_string_unhidden}\n{player_string}\nYou got 5 cards without busting! "
+                                      f"You win: £{bet * 2:,}")
+                    await self.update_user_balance(user_id, bet * 2)
+                    return
+            else:
+                await hit_or_stand.delete()
+                break
+        while dealer_total < 17:
+            dealer_hand.append(deck.pop())
+            dealer_total = sum(dealer_hand)
+            dealer_string += f", {dealer_hand[-1]}"
+            dealer_string_unhidden += f", {dealer_hand[-1]}"
+            if dealer_total > 21:
+                await ctx.respond(f"{dealer_string_unhidden}\n{player_string}\nThe dealer busted! You win: £{bet * 2:,}")
+                await self.update_user_balance(user_id, (bet * 2))
+                return
+        if dealer_total > player_total:
+            await ctx.respond(f"{dealer_string_unhidden}\n{player_string}\nThe dealer won! You lost: £{bet:,}")
+            await self.update_user_balance(user_id, 0 - bet)
+        elif dealer_total < player_total:
+            await ctx.respond(f"{dealer_string_unhidden}\n{player_string}\nYou won! You win: £{bet * 2:,}")
+            await self.update_user_balance(user_id, (bet * 2))
+        else:
+            await ctx.respond(f"{dealer_string_unhidden}\n{player_string}\nYou tied with the dealer! You win: £{bet:,}")
+            await self.update_user_balance(user_id, bet)
 
     async def add_user_db(self, userID, ctx):
         json_data = {
