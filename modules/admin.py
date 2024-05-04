dc25e7863c5d24117465dc4c7d9ae5bd3bf1a7e7
Davis <61391491+DavisThorne@users.noreply.github.com>
2024-05-05 00:54:29 +0100

    updated saves for muted users

--- modules/admin.py
+++ modules/admin.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands
 import random
 import numpy as np
+import csv
 import json
 import os
 
@@ -13,6 +14,8 @@ class Admin(commands.Cog, name="Admin"):
         self.client = client
         self.muted_logs_dir = os.path.join(os.getcwd(), "muted_logs")
         self.muted_users = []
+
+        self.load_muted_users()
 
     async def load_lookup_table(self, filename):
         file_path = os.path.join(self.lookup_dir, filename)
@@ -25,17 +28,28 @@ class Admin(commands.Cog, name="Admin"):
             f.write(data)
 
     async def save_muted_user(self, data, header="Muted Users"):
-        file_path = os.path.join(self.muted_logs_dir, "muted_users")
-        np.savetxt(file_path, data, delimiter=",", fmt='%s', header=header)
+        with open("muted_users.csv", "w", newline="") as f:
+            wr = csv.writer(f)
+            wr.writerow([header])
+            for user in data:
+                wr.writerow([user])
+
+    def load_muted_users(self):
+        with open("muted_users.csv", "r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                self.muted_users.append(row[0])
+            self.muted_users.pop(0)
+            return self.muted_users
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author in self.muted_users:
+        if str(message.author) in self.muted_users:
             date = datetime.now()
             date = date.strftime("%d/%m/%Y %H:%M:%S")
             data = f"\n{date}: {message.author} - {message.content}"
             await self.save_muted_log(f"{message.author}_muted_log.txt", data)
-        if message.author in self.muted_users:
+        if str(message.author) in self.muted_users:
             await message.delete()
 
     @commands.command(name="clear", description="Clears the chat")
@@ -53,7 +67,7 @@ class Admin(commands.Cog, name="Admin"):
         date = date.strftime("%d/%m/%Y %H:%M:%S")
         data = f"\n{date}: {member} has been muted"
         await self.save_muted_log(f"{member}_muted_log.txt", data)
-        self.muted_users.append(member)
+        self.muted_users.append(str(member))
         await self.save_muted_user(self.muted_users)
         await ctx.respond(f"{member} has been muted")
 
@@ -64,7 +78,7 @@ class Admin(commands.Cog, name="Admin"):
         date = date.strftime("%d/%m/%Y %H:%M:%S")
         data = f"\n{date}: {member} has been unmuted"
         await self.save_muted_log(f"{member}_muted_log.txt", data)
-        self.muted_users.remove(member)
+        self.muted_users.remove(str(member))
         await self.save_muted_user(self.muted_users)
         await ctx.respond(f"{member} has been unmuted")
 
